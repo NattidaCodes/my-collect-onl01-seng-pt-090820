dffbf43a07b9c09ad98f02d08790536ee03a3cff
Nattida Wong <nattidac.wong@gmail.com>
2020-09-18 23:12:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,4 @@
-# def my_collect(array)
-#   counter = 0
-#   new_array = []
-#
-#   while new_array << array.length
-#     yield(array[counter])
-#     array.upcase
-#   end
-# end
+
 
 def my_collect(array)
   array.collect do |name|
